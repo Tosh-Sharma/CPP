249b05d4abab3c938bc30e1edc627b6dab9cfd54
Tosh-Sharma <tosh.sharma@holocene.eu>
2024-08-19 09:37:26 +0200

    Updated main to have more readability.

--- CPP_07/ex02/main.cpp
+++ CPP_07/ex02/main.cpp
@@ -1,55 +1,64 @@
 #include "Array.hpp"
 
 int	main() {
-	std::cout << "Create empty int array:\n";
-	Array<int>	emptyInts;
-	std::cout << emptyInts << std::endl;
-	
-	std::cout << "\nCreate int array:\n";
-	Array<int> ints(3);
-	std::cout << ints << std::endl;
+	Array<int>			emptyArray;
+	Array<int>			integers(3);
+	Array<std::string>	strings(2);
 
-	std::cout << "\nFill it with '[]' operator:\n";
 	std::srand(std::time(0));
+
+	std::cout << "Initialized integers array: " << std::endl;
+	std::cout << integers << std::endl;
+	std::cout << "Size of integers array: " << integers.size() << std::endl;
+
 	for (int i = 0; i < 3; i++)
-		ints[i] = std::rand() % 100;
-	std::cout << ints << std::endl;	
+		integers[i] = std::rand() % 100;
 
-	std::cout << "\nFill emptyInts with '=' operator:\n";
-	emptyInts = ints;
-	std::cout << emptyInts << std::endl;
+	std::cout << "\nUpdated integers array: " << std::endl;
+	std::cout << integers << std::endl;	
 
-	std::cout << "\nModify values of ints with '[]' operator:\n";
+	std::cout << "Copy integers array to empty array with '=' operator:\n";
+	emptyArray = integers;
+	std::cout << "Filled Empty array: " << std::endl;
+	std::cout << emptyArray << std::endl;
+
+	std::cout << "\nModify values of integers array with [] operator:\n";
 	for (int i = 0; i < 3; i++)
-		ints[i] = std::rand() % 100;
+		integers[i] = std::rand() % 100;
 
-	std::cout << ints << std::endl;	
-	std::cout << emptyInts << std::endl;
+	std::cout << "Filled Integers array: " << std::endl;
+	std::cout << integers << std::endl;
+	std::cout << "Filled Empty array: " << std::endl;
+	std::cout << emptyArray << std::endl;
 
 	std::cout << "\nCreate strings and fill it with '[]' operator:\n";
-	Array<std::string>	strings(2);
 	strings[0] = "Hello";
 	strings[1] = "World";
-
-	std::cout << strings << std::endl;	
-
-	try {
-		strings[2] = "Superworld";
-	} catch (std::exception &e) {
-		std::cout << e.what() << std::endl;
-	}
-
-	std::cout << "\nCreate const copiedStrings by copy constructor:\n";
-	const Array<std::string>	copiedStrings(strings);
-
+	std::cout << "Original String array: " << std::endl;
 	std::cout << strings << std::endl;
 
 	std::cout << "Modify values of strings with '[]' operator:\n";
 	strings[0] = "Hello2";
 	strings[1] = "World2";
 
+	std::cout << "String array: " << std::endl;
 	std::cout << strings << std::endl;
+
+	std::cout << "\nTrying to access out of bound index of integers array:\n";
+	try {
+		integers[4] = 42;
+	} catch (std::exception &e) {
+		std::cout << "ERROR MESSAGE: ";
+		std::cout << e.what() << std::endl;
+	}
+
+	std::cout << "\nCreate copiedStrings by copy constructor:" << std::endl;
+	const Array<std::string>	copiedStrings(strings);
+
+	std::cout << "Original String array: " << std::endl;
+	std::cout << strings << std::endl;
+	std::cout << "\nCopied String array: " << std::endl;
 	std::cout << copiedStrings << std::endl;
 
-	return (0);
+	return 0;
 }
